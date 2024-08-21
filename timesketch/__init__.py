8654873ebacbdd23a62c7f2a286748b7d2db884f
nmadhavan-SFDC <41976394+nmadhavan-SFDC@users.noreply.github.com>
2024-08-22 09:38:18 +1000

    Update __init__.py

--- timesketch/__init__.py
+++ timesketch/__init__.py
@@ -13,11 +13,10 @@
 # limitations under the License.
 """Entry point for the application."""
 import logging
-from flask import request
-from flask_wtf import CSRFProtect
 
 def create_app(config=None):
     """Create the Flask app instance that is used throughout the application."""
+
     # Set up logging to a file
     handler = logging.FileHandler('timesketch.log')  # Log to 'timesketch.log'
     handler.setLevel(logging.DEBUG)  # Set the logging level
@@ -28,27 +27,13 @@ def create_app(config=None):
     logger = logging.getLogger(__name__)
     logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
+
     logger.info("Starting to create Flask application...")
 
     try:
         flask_app = app.create_app(config)
-        
-        # Setup CSRF protection for the whole application
-        csrf = CSRFProtect()
-        csrf.init_app(flask_app)
-
-        # Exempt API routes from CSRF protection
-        @flask_app.before_request
-        def csrf_exempt_api():
-            if request.path.startswith('/api/'):
-                csrf.exempt(request.view_function)
-            # Log request info
-            logger.debug('Headers: %s', request.headers)
-            logger.debug('Body: %s', request.get_data())
-
         logger.info("Flask application created successfully.")
         return flask_app
     except Exception as e:
         logger.error("Failed to create Flask application.", exc_info=True)
         raise e
-
